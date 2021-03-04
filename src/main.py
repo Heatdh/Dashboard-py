12737c7ad2b280e0ec64e90f5ce4e78d9e7cd541
rayengit config --global user.email ge36toc@tum.degit config --global user.name rayen <rayen.dhahri@tum.de>
2021-03-05 00:54:18 +0100

    git ignore not working

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+from pathlib import Path
 
-x = 10
-if x > 5 : 
-    print(5)
+df_vac1_repartion = pd.read_csv(Path(__file__).parent.parent /'dataset/first dosage.csv')
+df_vac2_repartion = pd.read_csv(Path(__file__).parent.parent /'dataset/second dosage.csv')
+print(df_vac2_repartion)
