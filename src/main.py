6653c97270af93ca67411aabece7804e8a3e2aa5
Blackshadow-GS <gabriela.sau.gs@gmail.com>
2021-03-12 15:24:49 +0100

    removing extra code to a draft py

--- src/main.py
+++ src/main.py
@@ -48,41 +48,7 @@ fig.update_xaxes(title_text='Date')
 fig.update_yaxes(type="log", title_text='Cases Last Week')
 
 
-'''ymax=max(max(BY["Cases_Last_Week"]),max(BE["Cases_Last_Week"]), max(NW["Cases_Last_Week"]), max(SN["Cases_Last_Week"]), max(TH["Cases_Last_Week"]) )
-state=""
-max_by= max(BY["Cases_Last_Week"])
-max_be=max(BE["Cases_Last_Week"])
-max_nw=max(NW["Cases_Last_Week"])
-max_sn=max(SN["Cases_Last_Week"])
-max_th=max(TH["Cases_Last_Week"])
-xmax=0
-if max_by==ymax :
-    state='BY'
-    s = BY["Cases_Last_Week"].idxmax()
-    xmax=BY.iloc[s,1]
-if max_be==ymax :
-    state='BE'
-    s = BE["Cases_Last_Week"].idxmax()
-    xmax=BE.iloc[s,1]
-if max_nw==ymax :
-    state='NW'
-    s = NW["Cases_Last_Week"].idxmax()
-    xmax=NW.iloc[s,1]
-if max_sn==ymax :
-    state='SN'
-    s = SN["Cases_Last_Week"].idxmax()
-    xmax=SN.iloc[s,1]
-if max_th==ymax :
-    state='TH'
-    s = TH["Cases_Last_Week"].idxmax()
-    xmax=TH.iloc[s,1]'''
 
-
-'''fig.add_annotation(x=xmax, y=ymax,
-            text='Maximum n={} in {} \n @{}'.format(ymax, state, xmax),
-            showarrow=True,
-            arrowhead=1,
-            yshift=ymax+10)'''
 fig.update_layout(title='7 day incidenz', title_x=0.5, template="simple_white")
 
 tumlogo = Path(__file__).parent.parent / 'assets/tumlogo.png'
